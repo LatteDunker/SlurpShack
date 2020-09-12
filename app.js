7807b2959622b17efc37f6963f631c4fc2d7bd4b
LatteDunker <59135856+LatteDunker@users.noreply.github.com>
2020-09-11 22:50:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,128 @@
+
+// Contentful CMS
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "m2z72uvohsnc",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "2N1L8FuQIEqU2Y3VOwPJ6mc3oYwVOxs6wR7wWStnJk0"
+  });
+console.log(client);
+
+///////////////////////////////////////////////////////////////////////////////////////////
+class Cart {
+    contents = [];
+
+    // Add item to cart
+    addItem(id, amount) {
+      let inCart = this.contents.find(item => item.id === id);
+  
+      //if id present in contents
+      if (inCart !== undefined) {
+        //increment by amount
+        inCart.amount += amount;
+        console.log("Item found");
+      }
+      //otherwise
+      else {  
+        //create cartItem w/ amount
+        let cartItem = {...Storage.getProduct(id)};
+        cartItem.amount = amount;
+        //push cartItem to contents
+        this.contents.push(cartItem);
+      }
+    }
+    
+    // Remove item from cart by id
+    removeItem(id) {
+        for (let i = 0; i < this.contents.length; i++) {
+            if (this.contents[i].id === id) {
+                this.contents.splice(i, 1);
+                console.log("removed");
+                return;
+            }
+        }
+    }
+
+    itemIncrement(id) {
+        for (let i = 0; i < this.contents.length; i++) {
+            if (this.contents[i].id === id) {
+                this.contents[i].amount++;
+                console.log("Increased");
+                return;
+            }
+        }
+    }
+
+    itemDecrement(id) {
+        for (let i = 0; i < this.contents.length; i++) {
+            if (this.contents[i].id === id) {
+                if (this.contents[i].amount === 1) {
+                    this.contents.splice(i, 1);
+                    console.log("Removed after decrease");
+                }
+                else {
+                    this.contents[i].amount--;
+                    console.log("decreased");
+                }
+                return
+            }
+        }
+    }
+
+}
+
+class Storage {
+    //Saves loaded products to local storage for attribute references
+    static saveProducts(products) {
+        localStorage.setItem("products", JSON.stringify(products));
+    }
+
+    //Downloads series of products from Contentful client and stores in local storage
+    static async loadProducts() {
+        try {
+            let contentful = await client.getEntries({content_type: "product"});
+            let products = contentful.items;
+
+            //destructuring contentful json format
+            products = products.map(item => {
+                const {category, title, price, id} = item.fields;
+                const image = item.fields.image.fields.file.url;
+                return {category, title, price, id, image};
+            })
+            Storage.saveProducts(products);
+            return products;
+        } 
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    // Saves cart for next session
+    static saveCart(cart) {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    }
+
+    // Retrieves cart from previous session
+    static getCart() {
+        if (localStorage.getItem('cart')) {
+            return JSON.parse(localStorage.getItem('cart'));
+        }
+        else {
+            return [];
+        }
+    }
+
+    // Retrieves product info from local storage
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('products'));
+        return products.find(item => item.id === id);
+    }
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+Storage.loadProducts();
+
+let cart = new Cart;
